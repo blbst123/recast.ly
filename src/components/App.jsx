f5a86f6b4972a049b37b7f46f4cc80bf3c7df7fc
blbst123 <billloukehao@gmail.com>
2019-04-22 16:47:04 -0700

    Implement App.jsx
    - Add imports and exports
    - Add VideoList component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import VideoList from './VideoList.js';
+import exampleVideoData from '../data/exampleVideoData.js';
+
 var App = () => (
   <div>
     <nav className="navbar">
@@ -8,9 +11,9 @@ var App = () => (
     <div className="row">
       <div className="col-md-7">
         <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+      </div>
+        <VideoList videos={exampleVideoData}/>
       </div>
     </div>
   </div>
@@ -18,4 +21,5 @@ var App = () => (
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
+ReactDOM.render(<App />, document.getElementById('app'));
 export default App;
