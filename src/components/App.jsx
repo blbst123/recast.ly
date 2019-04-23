75070f185173384ad90f6b0769a185128a06005d
blbst123 <billloukehao@gmail.com>
2019-04-22 19:29:01 -0700

    Implement onClick function for VideoListEntry component
    - The onClick function changes the main video displayed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,43 @@
 import VideoList from './VideoList.js';
-import exampleVideoData from '../data/exampleVideoData.js';
 import VideoPlayer from './VideoPlayer.js';
+import Search from './Search.js';
+import exampleVideoData from '../data/exampleVideoData.js';
 
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
-      </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <VideoPlayer video={exampleVideoData[0]}/>
-        <div className="col-md-5"></div>
-        <VideoList videos={exampleVideoData}/>
-      </div>
-    </div>
-  </div>
-);
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videoData : exampleVideoData,
+      currentVideo : 0
+    };
+    this.changeVideo = this.changeVideo.bind(this);
+  }
+
+  changeVideo(index) {
+    this.setState({
+      currentVideo: index
+    });
+  }
+
+  render() {
+    return (<div>
+      <nav className="navbar">
+        <div className="col-md-6 offset-md-3">
+          <Search />
+        </div>
+      </nav>
+      <div className="row">
+        <div className="col-md-7">
+          <VideoPlayer video={this.state.videoData[this.state.currentVideo]} />
+        </div>
+        <div className="col-md-5">
+          <VideoList changeVideo={this.changeVideo} videos={this.state.videoData} />
+        </div>
+      </div>
+    </div>);
+  }
+}
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 ReactDOM.render(<App />, document.getElementById('app'));
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,8 +1,8 @@
 import VideoListEntry from './VideoListEntry.js';
 
 var VideoList = (props) => {
-  var results = props.videos.map((element) =>
-    (<VideoListEntry video={element} />)
+  var results = props.videos.map((element, i) =>
+    (<VideoListEntry changeVideo={props.changeVideo} video={element} index={i} />)
   );
 
   return (<div className="video-list">{results}</div>);
